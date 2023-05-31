76331a3b173234237cf2bbfccc0bc0020328e2a1
dhruvin2004 <dhruvinpandav999@gmail.com>
2023-05-31 18:49:29 +0530

    31/05/2023 | ch:6 do while loop

---
+++ ch6/VC5-64.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,n1=0,n2=1,n3,n;
+	clrscr();
+	printf("enter your number: ");
+	scanf("%d",&n);
+	printf("%d %d " ,n1,n2);
+
+	for(i=1; i<=n-2; i++)
+	{
+		n3= n1+n2;
+		printf("%d ",n3);
+		n1=n2;
+		n2=n3;
+
+	}
+	getch();
+}
